a5dccd4148385c12284c63408cefdde3654003ba
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-06-14 21:09:17 +0530

    just testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [name,setName] = useState("");
+  function render() {
+       
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <ul>
+          <li>Tab 1</li>
+          <li>Tab 2</li>
+          <li>Tab 3</li>
+        </ul>
+        <p>This is content for {name}</p>
     </div>
   )
 }
